70b62dec5c1a902f321d2f03e0b985c6ac57a2c2
Coehl Gleckner (RIT Student) <crg5646@ad.rit.edu>
2017-12-08 16:59:47 -0500

    Change back
    this is going to be annoying isnt it...

--- puzzle.js
+++ puzzle.js
@@ -265,7 +265,18 @@
       output.innerHTML  = "beta / X : " + x + "\n";
       output.innerHTML += "gamma / Y: " + y + "\n";
 
+      // Because we don't want to have the device upside down
+      // We constrain the x value to the range [-90,90]
+      if (x >  90) { x =  90};
+      if (x < -90) { x = -90};
+
+      // To make computation easier we shift the range of 
+      // x and y to [0,180]
+      x += 90;
+      y += 90;
+
       position = movement(position, x, y);
+
     }
 
     function movement(pos, x, y){
